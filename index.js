3b874d03161a1def7cdbe0935234a1ca599ec34d
KjAnimates <kjhouser43@gmail.com>
2024-03-14 06:34:58 -0400

    Added CSS + Add Debug info for window.

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 let { app, BrowserWindow } = require("electron");
 
+let [ windowWidth, windowHeight ] = [ 800, 600 ]
+
 function createWindow() {
     let win = new BrowserWindow({
-        width: 800,
-        height: 600
+        width: windowWidth,
+        height: windowHeight,
     });
 
     win.loadFile("frontend/index.html");
+    
+    return win;
 }
 
 app.whenReady().then(() => {
-    createWindow()
+    let win = createWindow()
+
+    //Debug info
+    console.log("Created Window.");
+    console.log("Dimensions:");
+    console.log("   W: %d", windowWidth);
+    console.log("   H: %d", windowHeight);
+    console.log("Title: ", win.title);
 })
